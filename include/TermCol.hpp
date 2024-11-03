8b810bda072ee3c1df40c715b5335761af85b9b7
vebly <>
2024-11-03 22:25:41 +0100

    added terminal color struct

---
+++ include/TermCol.hpp
@@ -0,0 +1,11 @@
+#pragma once
+struct TermCol {
+    static constexpr const char* Reset = "\033[0m";
+    static constexpr const char* Red = "\033[31m";
+    static constexpr const char* Green = "\033[32m";
+    static constexpr const char* Yellow = "\033[33m";
+    static constexpr const char* Blue = "\033[34m";
+    static constexpr const char* Magenta = "\033[35m";
+    static constexpr const char* Cyan = "\033[36m";
+    static constexpr const char* White = "\033[37m";
+};
